60c9427d558bffbe328fcd1e55c5530b3f01eefb
Vitor Miracca <vitormiracca@gmail.com>
2024-04-04 20:39:08 -0300

    fix: folders

--- src/DBT/tests/test.sql
+++ src/DBT/tests/test.sql
@@ -5,8 +5,10 @@ WITH vendas_comparacao_preco AS (
         f.valor_venda,
         d.valor_sugerido,
         CASE
-            WHEN f.valor_venda <= d.valor_sugerido AND f.valor_venda >= d.valor_sugerido * 0.95 THEN TRUE
-            ELSE FALSE
+            WHEN f.valor_venda <= d.valor_sugerido AND f.valor_venda >= d.valor_sugerido * 0.95 THEN 
+                TRUE
+            ELSE 
+                FALSE
         END AS regra_respeitada
     FROM {{ ref('fct_vendas') }} f
     JOIN {{ ref('dim_veiculos') }} d ON f.veiculo_id = d.veiculo_id
