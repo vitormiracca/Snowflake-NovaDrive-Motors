38fe6615f2e1208f4f74ce0c187b2041bcd222fa
Vitor Miracca <vitormiracca@gmail.com>
2024-04-11 18:40:52 -0300

    feat: README

--- src/dag.py
+++ src/dag.py
@@ -9,7 +9,7 @@ default_args = {
     'start_date': datetime(2024, 1, 1),
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 0,
+    'retries': 1,
     'retry_delay': timedelta(minutes=1),
 }
  
